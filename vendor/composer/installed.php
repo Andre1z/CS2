8f10d457566b3517d2cbaaf0e9959367bb06ec5a
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2026-09-05 16:36:37 +0200

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cs2/inventory',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '29e04e65f677deb4fccd924e5ff7fbe1f63924ed',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cs2/inventory' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '29e04e65f677deb4fccd924e5ff7fbe1f63924ed',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
